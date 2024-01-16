09d980631c3719afa052954834dadd9c452aab06
Thomas Due <thdue@itm8.com>
2024-01-16 10:22:51 +0100

    A bit more formatting

--- source/trinity-continuum/library/public/js/statblock.js
+++ source/trinity-continuum/library/public/js/statblock.js
@@ -4,13 +4,23 @@ async function getUserData() {
     // Mock data
     if(typeof MapTool === typeof undefined) {
         let rawData = await fetch("./data/personae.json");
-        console.log(rawData);
         let personae = await rawData.json();
-        console.log(personae);
         return {
-            "action":"edit",
-            "data": personae[1]
+            action: "edit",
+            data: personae[4]
         }
+    }
+    else {
+        return MapTool.getUserData();
+    }
+}
+
+async function getSources() {
+    // Mock data
+    if(typeof MapTool === typeof undefined) {
+        let rawData = await fetch("./data/sources.json");
+        let sources = await rawData.json();
+        return sources;
     }
     else {
         return MapTool.getUserData();
@@ -29,6 +39,16 @@ function isHidden(id, value) {
     if (element != null) {
         element.hidden = value;
     }
+}
+
+function setEnhancements(id, data) {
+    const result = [];
+    for (const key in data) {
+        const value = Number(data[key]);
+        const prefix = (value > 0) ? "+" : "";
+        result.push(`${prefix}${value} (${key})`);
+    }
+    setValue(id, result.join(", "));
 }
 
 function setArray(spanId, array) {
