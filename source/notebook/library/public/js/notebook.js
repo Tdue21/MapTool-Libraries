6e6ee35fbdda2351bbd37394cf1ec9724ec57616
Thomas Due <thdue@itm8.com>
2024-02-06 16:11:34 +0100

    Micro changes

--- source/notebook/library/public/js/notebook.js
+++ source/notebook/library/public/js/notebook.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const { createApp } = Vue;
+
 createApp({
+    /* ********** Define model fields ********** */
     data() {
         return {
             action: 'show',
@@ -18,6 +20,7 @@ createApp({
         }
     },
 
+    /* ********** Define computed values ********** */
     computed: {
         doShow() {
             return { display: this.action === 'show' ? 'block' : 'none' }
@@ -29,7 +32,7 @@ createApp({
 
         summaryPage() {
             return {
-                id: this.uuidv4(),
+                id: "Summary",
                 name: "Summary",
                 content: this.summary
             }
@@ -38,14 +41,41 @@ createApp({
 
     methods: {
 
-        uuidv4() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-                .replace(/[xy]/g, function (c) {
-                    const r = Math.random() * 16 | 0,
-                        v = c == 'x' ? r : (r & 0x3 | 0x8);
-                    return v.toString(16);
-                });
+        /* ********** Define methods ********** */
+
+        async onInit() {
+
+            const rawdata = await MT.getUserData();
+            let userData = JSON.parse(atob(rawdata));
+            this.action = userData.action;
+            let notebook = JSON.parse(atob(userData.notebook));
+
+            this.title = notebook.title;
+            this.owner = notebook.owner;
+            this.accent = notebook.accent ? notebook.accent : "#A8A5CA";
+            this.colors = this.accentColors();
+            this.currentPage = await MD.parse(notebook.summary);
+
+            this.pages.push(this.summaryPage);
+
+            for (let page of notebook.pages) {
+
+                console.log(page.name);
+                this.pages.push({
+                    id: !page.id ? this.uuidv4() : page.id,
+                    name: page.name,
+                    content: page.content,
+                    uri: page.uri
+                })
+            }
+
+            let colors = this.accentColors(this.accent);
+            for (const key of Object.keys(colors)) {
+                document.documentElement.style.setProperty(key, colors[key]);
+            }
         },
+
+        /* ********** Define event handlers ********** */
 
         async editNotebook(event) {
             this.pages = this.pages.slice(1);
@@ -99,6 +129,8 @@ createApp({
             }
         },
 
+        /* ********** Define utility functions ********** */
+
         accentColors(color) {
             let tc = tinycolor(color);
             let background = tc.toHexString();
@@ -115,130 +147,18 @@ createApp({
             };
             return colorData;
         },
-
-        async onInit() {
-
-            const rawdata = await MT.getUserData();
-            let userData = JSON.parse(atob(rawdata));
-            this.action = userData.action;
-            let notebook = JSON.parse(atob(userData.notebook));
-
-            this.title = notebook.title;
-            this.owner = notebook.owner;
-            this.accent = notebook.accent ? notebook.accent : "#A8A5CA";
-            this.colors = this.accentColors();
-            this.currentPage = await MD.parse(notebook.summary);
-
-            this.pages.push(this.summaryPage);
-
-            for (let page of notebook.pages) {
-
-                console.log(page.name);
-                this.pages.push({
-                    id: this.uuidv4(),
-                    name: page.name,
-                    content: page.content,
-                    uri: page.uri
-                })
-            }
-
-            let colors = this.accentColors(this.accent);
-            for (const key of Object.keys(colors)) {
-                document.documentElement.style.setProperty(key, colors[key]);
-            }
+        
+        uuidv4() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+                .replace(/[xy]/g, function (c) {
+                    const r = Math.random() * 16 | 0,
+                        v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                });
         }
+
     },
     mounted() {
         this.onInit();
     }
 }).mount('#app');
-
-/*
-(async () => {
-    const rawdata = await MT.getUserData();
-
-    function getColors(color) {
-        let tc = tinycolor(color);
-        let background = tc.toHexString();
-        let hover = tc.darken(10).toHexString();
-        let active = tc.darken(20).toHexString();
-
-        return {
-            'background': {
-                'color': background,
-                'hover': hover,
-                'active': active,
-            },
-            'foreground': {
-                'color': tc.isLight() ? 'black' : 'white',
-                'hover': tinycolor(hover).isLight() ? 'black' : 'white',
-                'active': tinycolor(active).isLight() ? 'black' : 'white'
-            }
-        };
-    }
-
-    try {
-        let userData = JSON.parse(atob(rawdata));
-
-        let root = document.documentElement;
-        let colors = getColors(userData.accent ? userData.accent : "#a8a5ca");
-
-        root.style.setProperty('--accent-bg', colors.background.color);
-        root.style.setProperty('--accent-hover-bg', colors.background.hover);
-        root.style.setProperty('--accent-active-bg', colors.background.active);
-
-        root.style.setProperty('--accent-fg', colors.foreground.color);
-        root.style.setProperty('--accent-hover-fg', colors.foreground.hover);
-        root.style.setProperty('--accent-active-fg', colors.foreground.active);
-        
-        let title = document.getElementById("bookTitle");
-        let summary = document.getElementById("summaryEntry");
-        let owner = document.getElementById("footerOwner");
-
-        let indexPanel = document.getElementById("indexPanel");
-        let pageIndex = document.getElementById("pageIndex");
-        let pagePanel = document.getElementById("pagePanel");
-        let editBook = document.getElementById("editBook");
-
-        pagePanel.innerHTML = await MD.parse(userData.summary);
-
-        title.innerText = userData.title;
-        owner.innerText = userData.owner;
-       
-        summary.addEventListener("click", async event => pagePanel.innerHTML = await MD.parse(userData.summary));
-        userData.pages.forEach(page => pageIndex.appendChild(createPageLink(page.name)));
-
-        function createPageLink(pageName) {
-            let link = Utils.createElement("span", {
-                id: btoa(pageName),
-                innerText: pageName
-            });
-
-            let item = Utils.createElement("li", null, [
-                Utils.createElement("div", { className: "entry" }, [link])
-            ]);
-
-            link.addEventListener("click", async event => {
-                await showPage(event.target.id);
-            });
-
-            return item;
-        }
-
-        async function showPage(pageId) {
-            let name = atob(pageId);
-            let page = userData.pages.find(page => page.name === name);
-            if (page.uri) {
-                let rawdata = await fetch(page.uri);
-                let content = await rawdata.text();
-                pagePanel.innerHTML = await MD.parse(content);
-            } else {
-                pagePanel.innerHTML = await MD.parse(page.content);
-            }
-        }
-        
-    } catch (error) {
-        console.error(error.stack);
-    }
-})();
-*/
