1c8b5b9c1af94fc07cbe622d849f137b2745b965
tdue21 <tho.due@gmail.com>
2024-04-01 14:20:27 +0200

    Script for starting sass watcher

---
+++ build/start-sass.cmd
@@ -0,0 +1,4 @@
+sass --watch ^
+.\source\Pins\library\public\css\sass\:.\source\Pins\library\public\css\ ^
+.\source\notebook\library\public\css\sass\:.\source\Pins\library\public\css\ ^
+.\source\trinity-continuum\library\public\css\sass\:.\source\trinity-continuum\library\public\css\
